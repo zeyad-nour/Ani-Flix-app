92a0c06020aeea4c10116d6d0e17dc1b3301166e
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-26 15:56:37 +0300

    set base color and set base text style and create core layer

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const kBackgroundColor = Color(0xff18181B);
+const colorfontRegularAndBoldbase = Color(0xffFFFFFF);
+const colorFontRegularsecound = Color(0xff848484);
+const colorFontRegularwhite = Color(0xffE8E8E8);
+const colorFontRegulartitle = Color(0xff868686);
+
---
+++ lib/core/utils/style.dart
@@ -0,0 +1,28 @@
+import 'package:aniflix_app/constant.dart';
+import 'package:flutter/cupertino.dart';
+
+abstract class Style { 
+    static const textStyle14 = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+    static const textStyle12 = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: colorFontRegularsecound,
+
+  );
+  
+    static const textStyle16 = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+   color: colorfontRegularAndBoldbase,
+
+  );
+
+
+
+}
