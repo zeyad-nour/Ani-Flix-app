ee192e36929b4296fc644865ea830d5df0986e95
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-26 16:01:57 +0300

    create Feature layer and Home view

---
+++ lib/Features/home/presentation/views/home_views.dart
@@ -0,0 +1,18 @@
+import 'package:aniflix_app/Features/home/presentation/views/widgets/Home_Views_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeViews extends StatefulWidget {
+  const HomeViews({super.key});
+
+  @override
+  State<HomeViews> createState() => _HomeViewsState();
+}
+
+class _HomeViewsState extends State<HomeViews> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      body: HomeViewsBody(),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/Home_Views_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeViewsBody extends StatelessWidget {
+  const HomeViewsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column( 
+      
+    );
+  }
+}
