6178c6ff5f6f07354497ad904d46cc5342b8895e
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-26 16:30:04 +0300

    finished from Appbar

--- lib/Features/home/presentation/views/widgets/customAppBar.dart
+++ lib/Features/home/presentation/views/widgets/customAppBar.dart
@@ -8,42 +8,47 @@ class Customappbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          margin: EdgeInsets.only(left: 24),
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-            color: colorFontRegulartitle,
-            borderRadius: BorderRadius.circular(250),
-            image: DecorationImage(
-              image: AssetImage(AssetsData.imageuser),
-              fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 24),
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              color: colorFontRegulartitle,
+              borderRadius: BorderRadius.circular(250),
+              image: DecorationImage(
+                image: AssetImage(AssetsData.imageuser),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        SizedBox(width: 14),
-        Column(
-          children: [
-            Text(
-              "Welcome back",
-              style: Style.textStyle14.copyWith(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
+          SizedBox(width: 14),
+          Column(
+            children: [
+              Text(
+                "Welcome back",
+                style: Style.textStyle14.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            Text(
-              "Maria",
-              style: Style.textStyle14.copyWith(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
+              Text(
+                "Maria",
+                style: Style.textStyle14.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-          ],
-        ),
-        Image.asset(AssetsData.appBarIcon),
-      ],
+            ],
+          ),
+          // Image.asset(AssetsData.appBarIcon),
+          Spacer(),
+          Icon(Icons.list, size: 40),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(darkTheme: ThemeData.dark(), home: HomeViews());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      darkTheme: ThemeData.dark(),
+      home: HomeViews(),
+    );
   }
 }
