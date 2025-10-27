7041fcdb1005ababf8310e7c71f875f39102f5c5
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-28 00:51:17 +0300

    Done From UI and Linked Navigation  logic

--- lib/Features/details/presentation/widgets/custom_button.dart
+++ lib/Features/details/presentation/widgets/custom_button.dart
@@ -1,3 +1,4 @@
+import 'package:aniflix_app/constant.dart';
 import 'package:flutter/material.dart';
 
 class GradientButton extends StatelessWidget {
@@ -5,27 +6,39 @@ class GradientButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            colors: [Color(0xFF8A2BE2), Color(0xFF00FFFF)],
-          ),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        padding: const EdgeInsets.all(2),
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xFF1A1A1A),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 14),
-          child: const Text(
-            'Watch now',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 110.0),
+      child: MaterialButton(
+        elevation: 2,
+        splashColor: kBackgroundColor,
+        clipBehavior: Clip.antiAlias,
+        onPressed: () {},
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Color(0xFF8A2BE2), Color(0xFF00FFFF)],
+              ),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: const EdgeInsets.all(2),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF1A1A1A),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 100,
+                vertical: 14,
+              ),
+              child: const Text(
+                'Watch now',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ),
           ),
         ),
