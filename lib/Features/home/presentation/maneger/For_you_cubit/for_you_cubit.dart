e532a606dcdc4b398cc449abe2ed162febc55bcc
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-29 16:31:24 +0300

    Create Cubite to Setion For you video

--- lib/Features/home/presentation/maneger/For_you_cubit/for_you_cubit.dart
+++ lib/Features/home/presentation/maneger/For_you_cubit/for_you_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:aniflix_app/Features/home/data/model/anime_model/anime_model.dart';
+import 'package:aniflix_app/Features/home/data/repo/home_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'for_you_state.dart';
 
 class ForYouCubit extends Cubit<ForYouState> {
-  ForYouCubit() : super(ForYouInitial());
+  ForYouCubit(this.homeRepo) : super(ForYouInitial());
+  final HomeRepo homeRepo;
+  Future<void> featchSuggetions() async {
+    emit(ForYouLoding());
+
+    var response = await homeRepo.featchSuggetion();
+
+    response.fold(
+      (Failure) {
+        emit(ForYouFailure(Failure.errorMessage));
+      },
+      (foryouVideos) {
+        print(foryouVideos.first.title);
+        print(foryouVideos.first.images?.jpg?.imageUrl);
+        emit(ForYouSuccess(foryouVideos));
+      },
+    );
+  }
 }
